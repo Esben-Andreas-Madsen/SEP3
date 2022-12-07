e24b41e15812323b70644e8db6bc9347477f74c7
benjjjjjj <302301@viauc.dk>
2022-12-07 11:46:17 +0100

    hej

---
+++ HttpClients/ClientInterfaces/IUserStoryService.cs
@@ -0,0 +1,11 @@
+﻿using Shared.DTOs;
+using Shared.Models;
+
+namespace HttpClients.ClientInterfaces;
+
+public interface IUserStoryService
+{
+    Task<UserStory> Create(UserStoryCreationDto dto);
+    
+    Task<IEnumerable<UserStory>> GetUsers();
+}
---
+++ HttpClients/Implementations/UserStoryHttpClient.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Http.Json;
+using System.Text.Json;
+using HttpClients.ClientInterfaces;
+using Shared.DTOs;
+using Shared.Models;
+
+namespace HttpClients.Implementations;
+
+public class UserStoryHttpClient : IUserStoryService
+{
+    
+    private readonly HttpClient client;
+    
+    public UserStoryHttpClient(HttpClient client)
+    {
+        this.client = client;
+    }
+    public async Task<UserStory> Create(UserStoryCreationDto dto)
+    {
+        HttpResponseMessage response = await client.PostAsJsonAsync("/userStory", dto);
+        string result = await response.Content.ReadAsStringAsync();
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception(result);
+        }
+
+        UserStory userStory = JsonSerializer.Deserialize<UserStory>(result, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        })!;
+        return userStory;
+    }
+
+    public Task<IEnumerable<UserStory>> GetUsers()
+    {
+        throw new NotImplementedException();
+    }
+}
