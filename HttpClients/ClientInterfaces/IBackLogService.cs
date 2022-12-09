2c4c65876db74a2905775505bc04baced91db65f
Esben-Andreas-Madsen <316460@via.dk>
2022-12-09 09:16:35 +0100

    Deleted unused classes Fixed chrome source null error

--- HttpClients/ClientInterfaces/IBackLogService.cs
+++ HttpClients/ClientInterfaces/IBackLogService.cs
@@ -7,6 +7,4 @@ public interface IBackLogService
 {
     Task CreateAsync(BacklogCreationDto dto);
     Task<IEnumerable<Backlog>?> GetAsync();
-    Task UpdateAsync(BacklogUpdateDto dto);
-    Task DeleteAsync(string name);
 }
