7ca5dffe7a682b9f3f20154dc160ab9ef79c07d5
Esben-Andreas-Madsen <316460@via.dk>
2022-12-12 12:06:23 +0100

    user creation validatedata() updated

--- Application/Logic/UserLogic.cs
+++ Application/Logic/UserLogic.cs
@@ -16,13 +16,35 @@ public class UserLogic : IUserLogic
 
     private static void ValidateData(UserCreationDto dto)
     {
-        string? userName = dto.UserName;
+        string userName = dto.UserName;
+        string password = dto.Password;
+        string role = dto.Role;
+        string fname = dto.FirstName;
+        string lname = dto.LastName;
 
-        if (userName.Length < 3)
-            throw new Exception("Username must be at least 3 characters!");
 
+        if (userName.Length < 7)
+            throw new Exception("Username must be at least 7 characters!");
         if (userName.Length > 15)
             throw new Exception("Username must be less than 16 characters!");
+
+        if (password.Length < 7)
+            throw new Exception("Password must be at least 7 characters!");
+        if (password.Length > 15)
+            throw new Exception("Password must be less than 15 characters!");
+        
+        if (role.Length < 4)
+            throw new Exception("Role must be at least 4 characters!");
+        
+        if (fname.Length < 3)
+            throw new Exception("Firstname must be at least 3 characters!");
+        if (fname.Length > 40)
+            throw new Exception("Firstname must be less than 40 characters!");
+        
+        if (lname.Length < 3)
+            throw new Exception("Lastname must be at least 3 characters!");
+        if (lname.Length > 40)
+            throw new Exception("Lastname must be less than 40 characters!");
     }
 
     public Task<UserCreationDto> CreateAsync(UserCreationDto dto)
--- Shared/Auth/AuthorizationPolicies.cs
+++ Shared/Auth/AuthorizationPolicies.cs
@@ -19,7 +19,7 @@ public class AuthorizationPolicies
         services.AddAuthorizationCore(options =>
         {
             options.AddPolicy("MustBeScrumMember", a =>
-                a.RequireAuthenticatedUser().RequireClaim("Role", "Scrum Member",  "Scrum Master"));
+                a.RequireAuthenticatedUser().RequireClaim("Role", "Scrum Member", "Scrum Master"));
         });
     }
 }
